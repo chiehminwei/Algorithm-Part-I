8833d2a578574cd4e52d89c8827d0decfbe23399
Jimmy Wei <cmw025@bucknell.edu>
2017-08-27 23:04:03 -0400

    Lab02, Exercise 3, complete

--- src/lab02/FtoC.java
+++ src/lab02/FtoC.java
@@ -20,17 +20,37 @@ import java.util.Scanner;
 
 
 public class FtoC {
-    public static void main(String[] args) {
-    	
-        // Prompt user for a Fahrenheit temp
-    	System.out.print("Enter a temperature in Fahrenheit: ");
-        // Create the Scanner object, attached to console input
-    	Scanner in = new Scanner(System.in);
-        // Read and store the Fahrenheit temp
-    	int fahTemp = in.nextInt();
-        // Convert the temp to Celsius
-    	double celTemp = 5.0 / 9.0 * (fahTemp - 32);
-        // Output the results with correct formatting
-    	System.out.println(String.format("%d F = %.1f C", fahTemp, celTemp));
-    }
+	public static void main(String[] args) {
+
+		while (true) {
+        	// Prompt user for a Fahrenheit temp
+			System.out.print("Enter a temperature in Fahrenheit: ");
+        	// Create the Scanner object, attached to console input
+			Scanner in = new Scanner(System.in);
+       		 // Read and store the Fahrenheit temp
+			int fahTemp = in.nextInt();
+        	// Convert the temp to Celsius
+			double celTemp = 5.0 / 9.0 * (fahTemp - 32);
+        	// Output the results with correct formatting
+			System.out.println(String.format("%d F = %.1f C", fahTemp, celTemp));
+
+    		// Branching
+			if (celTemp <= 0) {
+				System.out.println("Brrr... it is FREEZING out!");
+			}
+			else if (celTemp <= 15) {
+				System.out.println("It's a bit cool out");
+			}
+			else if (celTemp <= 30) {
+				System.out.println("It's comfortably warm");
+			}
+			else {
+				System.out.println("It's HOT! I need A/C!");
+			}
+			System.out.print("Try again? [y | n]: ");
+			if (!in.next().equalsIgnoreCase("y"))
+				break;
+		}
+		System.out.println("Goodbye!");
+	}
 }
